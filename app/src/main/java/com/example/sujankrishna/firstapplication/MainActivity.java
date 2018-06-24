76bee90c3ba161d60f317c125e0bf50bd795f43d
Sujan Krishna Joshi <skj.jo0c@gmail.com>
2018-06-24 11:01:30 +0545

    on click functionality

--- app/src/main/java/com/example/sujankrishna/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/sujankrishna/firstapplication/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.sujankrishna.firstapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +20,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         etID = (EditText) findViewById(R.id.etID);
-        tv = findViewById(R.id.tv);
+        tv = (TextView) findViewById(R.id.tv);
+    }
+
+    public void btnSubmit_OnClick(View v){
+        String data = etID.getText().toString();
+        String dob = data.substring(0,8);
+
+        int gender = Integer.parseInt(data.charAt(8) + "");
+        String sGender;
+
+        if(gender >= 0){
+            sGender = "Male";
+        }
+        else{
+            sGender = "Female";
+        }
+
+        int nationality = Integer.parseInt(data.charAt(9) + "");
+        String sNationality;
+
+        if(nationality == 0){
+            sNationality = "Foreigner";
+        }
+        else{
+            sNationality = "Nepali";
+        }
+
+
+        tv.setText("Your information from your id number: \n"
+                    + "Date of Birth: \t" + dob + "\n"
+                    + "Gender: \t" + sGender + "\n"
+                    + "Nationality: \t" + sNationality);
+
+        etID.onEditorAction(EditorInfo.IME_ACTION_DONE);
     }
 }
